9e2cce06f0561166b2a487baa51d560fbcb67f5e
miguelmercado24 <32377727+miguelmercado24@users.noreply.github.com>
2018-04-09 18:32:23 -0700

    Porject setup

---
+++ CMPM265_Hmw01Particle_Effects/CMPM265_Hmw01Particle_Effects/main.cpp
@@ -0,0 +1,36 @@
+//Miguel Ocana Mercado
+//mocaname@ucsc.edu
+//CMPM 265
+//HMK01 Particle Systems
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+#include <SFML/OpenGL.hpp>
+#include <SFML/Main.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
